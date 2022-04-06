f1460b79013fe3acf09bb7a00a4dd05755e0bf6f
David <david.pichard@etu.univ-lyon1.fr>
2022-04-06 13:30:45 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./styles.css";
 
-function App() {
+const App = () => {
+  const [newElement, setNewElement] = useState("");
+  const [elements, setElements] = useState([]);
+
+  const [searchElement, setSearchElement] = useState("");
+
+  const addElement = (event) => {
+    if (event.key === "Enter") {
+      const element = {
+        value: newElement,
+      };
+      setElements((oldList) => [...oldList, element]);
+      setNewElement("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrap">
+      <div className="milieu">
+        <div className="barre">
+          <input
+            type="text"
+            onChange={(event) => setNewElement(event.target.value)}
+            value={newElement}
+            onKeyPress={addElement}
+            placeholder="Add an element"
+          />
+
+          <input
+            type="text"
+            onChange={(event) => setSearchElement(event.target.value)}
+            placeholder="Search an element"
+          />
+        </div>
+
+        <div className="result">
+          {elements
+            .filter((element) => {
+              if (
+                element.value
+                  .toLowerCase()
+                  .includes(searchElement.toLowerCase())
+              ) {
+                return element;
+              }
+            })
+            .map((element) => {
+              return <p>{element.value}</p>;
+            })}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
